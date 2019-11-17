351b1585128f335dc05f5443c081b99bd08ac5d8
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-11-17 19:24:29 +0100

    working afl-frida-spawn on linux x64

--- frida_mode/afl-frida-spawn.c
+++ frida_mode/afl-frida-spawn.c
@@ -24,6 +24,13 @@ static size_t req_id;
 static char msg_buf[200];
 
 static guint target_pid;
+static int shm_id;
+
+static int tgt_argc;
+static char** tgt_argv;
+static char* input_filename;
+
+static char* agent_code;
 
 static FridaSession * session;
 static FridaScript * script;
@@ -95,14 +102,80 @@ void tohex(unsigned char * in, size_t insz, char * out)
     pout[0] = 0;
 }
 
+static void respawn(void) {
+
+  g_print (" >> Try to spawn %s on %s\n", tgt_argv[0], frida_device_get_name (local_device));
+  
+  FridaSpawnOptions *spawn_options = frida_spawn_options_new ();
+  if (tgt_argc > 1)
+    frida_spawn_options_set_argv (spawn_options, tgt_argv, tgt_argc);
+  target_pid = frida_device_spawn_sync (local_device, tgt_argv[0], spawn_options, NULL, &error);
+  g_object_unref (spawn_options);
+
+  if (error) {
+    g_printerr ("Failed to spawn: %s\n", error->message);
+    clean_exit(1);
+  }
+
+  g_print ("[*] Spawned with pid %d\n", target_pid); 
+ 
+  session = frida_device_attach_sync (local_device, target_pid, NULL, &error);
+  if (error == NULL)
+  {
+    FridaScriptOptions * options;
+
+    g_print ("[*] Attached to pid %d\n", target_pid);
+
+    options = frida_script_options_new ();
+    frida_script_options_set_name (options, "afl-frida-agent");
+    frida_script_options_set_runtime (options, FRIDA_SCRIPT_RUNTIME_V8);
+    
+    script = frida_session_create_script_sync (session, agent_code, options,
+                                               NULL, &error);
+    g_assert (error == NULL);
+
+    g_clear_object (&options);
+    
+    g_print ("[*] Script created\n");
+
+    frida_script_load_sync (script, NULL, &error);
+    g_assert (error == NULL);
+
+    g_print ("[*] Script loaded\n");
+    
+    size_t in_file_len = strlen(input_filename);
+    char * in_file_hex = malloc(in_file_len * 2 + 1);
+    
+    tohex(input_filename, in_file_len, in_file_hex);
+    
+    sprintf(msg_buf,
+            "[\"frida:rpc\", %lu, \"call\", \"setup\", [%d, \"%s\", %llu, %lu]]",
+             ++req_id, shm_id, in_file_hex, TARGET_FUNCTION, PAYLOAD_MAX_LEN);
+
+    frida_script_post_sync(script, msg_buf, NULL, NULL, &error);
+    if (error != NULL) clean_exit(1);
+    
+  } else {
+  
+    g_printerr ("Failed to attach: %s\n", error->message);
+    g_error_free (error);
+
+    clean_exit(1);
+
+  }
+
+}
+
 static void mimic_forkserver() {
 
   static unsigned char tmp[4];
   pid_t                child_pid;
   int fake_status = 0;
 
+  g_print(" >> Starting fake forkserver\n");
+
   if (write(FORKSRV_FD + 1, tmp, 4) != 4) {
-    g_print(" >> No forkserver, no party\n");
+    g_printerr(" >> No forkserver, no party\n");
     return;
   }
 
@@ -122,6 +195,11 @@ static void mimic_forkserver() {
     
     if (error != NULL) {
       fake_status = 134; // abort
+      g_print("Crash! %s\n", error->message);
+      g_error_free(error);
+      error = NULL;
+
+      respawn();
     }
 
     // send child stop status to afl-fuzz
@@ -140,7 +218,6 @@ main (int argc, char * argv[])
   FridaDeviceList * devices;
   gint num_devices, i;
 
-  int shm_id;
   char *id_str = getenv(SHM_ENV_VAR);
   if (!id_str) {
     g_printerr (SHM_ENV_VAR " not defined!\n", argv[0]);
@@ -156,6 +233,10 @@ main (int argc, char * argv[])
     g_printerr ("Usage: %s <input filename> <target program> [target args...]\n", argv[0]);
     return 1;
   }
+
+  input_filename = argv[1];
+  tgt_argc = argc -2;
+  tgt_argv = argv +2;
 
   manager = frida_device_manager_new ();
 
@@ -181,67 +262,16 @@ main (int argc, char * argv[])
   devices = NULL;
   
   size_t agent_code_len;
-  char * agent_code = read_file("afl-frida-agent.js", &agent_code_len);
+  agent_code = read_file("afl-frida-agent.js", &agent_code_len);
   
   if (!agent_code) {
     g_printerr ("Failed to open afl-frida-agent.js\n");
     return 1;
   }
-  
-  g_print (" >> Try to spawn %s on %s\n", argv[2], frida_device_get_name (local_device));
-  
-  FridaSpawnOptions *options = frida_spawn_options_new ();
-	const int tgt_argc = argc -2;
-	if (tgt_argc > 1)
-		frida_spawn_options_set_argv (options, argv +2, tgt_argc);
-	target_pid = frida_device_spawn_sync (local_device, argv[2], options, NULL, &error);
-	g_object_unref (options);
-  
-  if (error == NULL)
-  {
-    FridaScriptOptions * options;
+ 
+  respawn();
 
-    g_print ("[*] Spawned with pid %d\n", target_pid);
-
-    options = frida_script_options_new ();
-    frida_script_options_set_name (options, "afl-frida-agent");
-    frida_script_options_set_runtime (options, FRIDA_SCRIPT_RUNTIME_V8);
-    
-    script = frida_session_create_script_sync (session, agent_code, options,
-                                               NULL, &error);
-    g_assert (error == NULL);
-
-    g_clear_object (&options);
-    
-    g_print ("[*] Script created\n");
-
-    frida_script_load_sync (script, NULL, &error);
-    g_assert (error == NULL);
-
-    g_print ("[*] Script loaded\n");
-    
-    size_t in_file_len = strlen(argv[1]);
-    char * in_file_hex = malloc(in_file_len * 2 + 1);
-    
-    tohex(argv[1], in_file_len, in_file_hex);
-    
-    sprintf(msg_buf,
-            "[\"frida:rpc\", %lu, \"call\", \"setup\", [%d, \"%s\", %llu, %lu]]",
-             ++req_id, shm_id, in_file_hex, TARGET_FUNCTION, PAYLOAD_MAX_LEN);
-
-    frida_script_post_sync(script, msg_buf, NULL, NULL, &error);
-    if (error != NULL) clean_exit(1);
-    
-    g_print(" >> Starting fake forkserver\n");
-    
-    mimic_forkserver();
-
-  }
-  else
-  {
-    g_printerr ("Failed to attach: %s\n", error->message);
-    g_error_free (error);
-  }
+  mimic_forkserver();
   
   g_print ("[*] Exiting\n");
 
